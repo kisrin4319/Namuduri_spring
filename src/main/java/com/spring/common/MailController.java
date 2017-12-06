18c012eeb4bea6063e42554b933a26c32dbee591
kisrin4319 <kisrin4319>
2017-12-07 01:54:28 +0900

    usedbook 폴더명 변경

--- src/main/java/com/spring/common/MailController.java
+++ src/main/java/com/spring/common/MailController.java
@@ -8,7 +8,6 @@ import javax.servlet.http.HttpServletRequest;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
